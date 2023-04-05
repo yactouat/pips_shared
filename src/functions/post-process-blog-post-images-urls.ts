d9da5c4c70947f92fb87495a9ea3ebc4d2bff2c7
yactouat <yactouat@hotmail.com>
2023-04-05 21:32:50 +0200

    adding images urls post processing function

---
+++ src/functions/post-process-blog-post-images-urls.ts
@@ -0,0 +1,11 @@
+const postProcessBlogPostImagesUrls = (
+  blogPostStr: string,
+  imagesBaseUrl: string
+): string => {
+  return blogPostStr.replace(
+    /<img src="\.\/\.\.\/images/g,
+    '<img class="content-images" src="' + imagesBaseUrl
+  );
+};
+
+export default postProcessBlogPostImagesUrls;
