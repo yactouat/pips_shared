ec280e826aeeb2fa91c71db610d23bdf99d76d0d
yactouat <yactouat@hotmail.com>
2023-05-01 13:54:57 +0200

    refining blog posts post processing function

--- src/functions/post-process-blog-post-images-urls.ts
+++ src/functions/post-process-blog-post-images-urls.ts
@@ -2,10 +2,15 @@ const postProcessBlogPostImagesUrls = (
   blogPostStr: string,
   imagesBaseUrl: string
 ): string => {
-  return blogPostStr.replace(
-    /<img src="\.\/\.\.\/images/g,
-    '<img class="content-images" src="' + imagesBaseUrl
-  );
+  return blogPostStr
+    .replace(
+      /<img src="\.\/\.\.\/images/g,
+      '<img class="content-images" src="' + imagesBaseUrl
+    )
+    .replace(
+      /<img src="\.\.\/images/g,
+      '<img class="content-images" src="' + imagesBaseUrl
+    );
 };
 
 export default postProcessBlogPostImagesUrls;
